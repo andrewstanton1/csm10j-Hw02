c9a82ad1c7bb9dcf1c9abe4d026c3063d152747c
andrewstanton1 <andrewstanton020@gmail.com>
2015-11-09 19:37:56 -0800

    Create Car.java

---
+++ InheritanceDesign/Car.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InheritanceDesign;
+
+/**
+ *
+ * @author andrew
+ */
+public class Car extends Property{
+    
+    public Car(String name, double price, double financed){
+        super(name, price, financed);
+    }
+    
+    @Override
+    public double getAssetValue(){
+        return getValueAmount();
+    }
+    
+    @Override
+    public String toString() {
+        String className = this.getClass().getSimpleName();
+        double totalValue = getValueAmount();
+        double totalDebt = getDebtAmount();
+        String value = String.format("Value: $%,.2f", totalValue);
+        String financed = String.format("Debt: $%,.2f", totalDebt);
+        
+        return name + ", " + className + ", " + value + ", " + financed;
+    }
+}

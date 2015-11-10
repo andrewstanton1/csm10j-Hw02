6570575b7d3a76396de67f3eb337a6df5c1fb320
andrewstanton1 <andrewstanton020@gmail.com>
2015-11-09 19:58:06 -0800

    Create Security.java

---
+++ src/InheritanceDesign/Security.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InheritanceDesign;
+
+/**
+ *
+ * @author andre
+ */
+abstract public class Security extends Asset{
+    public double price;
+    public int quantity;
+    
+    public Security (String name, double price, int quantity)
+    {
+        super(name);
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
